90f7ac40d0dbcdd971484a28fde70a098beaef6c
JohnDaise <jdaise@gmail.com>
2023-05-31 23:24:57 -0700

    fix to make sure food doesnt refresh too close to the borders

--- food.py
+++ food.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 import random
 
+
 class Food(Turtle):
     def __init__(self):
         super().__init__()
@@ -9,12 +10,12 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_x = random.randint(-260, 260)
+        random_y = random.randint(-260, 260)
         self.goto(random_x, random_y)
         self.refresh()
 
     def refresh(self):
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_x = random.randint(-260, 260)
+        random_y = random.randint(-260, 260)
         self.goto(random_x, random_y)
